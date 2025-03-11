7e62238c36ae3062a073438fbcb8ddef90ea78f4
milap <milap@softnweb.in>
2025-03-11 22:40:20 +0530

    🔧 Improve Laravel CI/CD: Fix PHP-CS-Fixer & Optimize Checks

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,7 +12,7 @@ class User extends Authenticatable
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
-    protected $fillable = [
+protected $fillable = [
         'name',
         'email',
         'password',
