7de10cc96cb14848219128ddf50a237e01076952
milap <milap@softnweb.in>
2025-03-11 22:37:49 +0530

    Added formatting and docblock issues

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,11 +12,6 @@ class User extends Authenticatable
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
         'name',
         'email',
